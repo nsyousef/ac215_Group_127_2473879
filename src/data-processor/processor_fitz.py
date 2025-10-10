20b6e2dbdded7d7b67770fe2aa8ac7369f35888e
Nicholas Yousefi <nsyousef@ucsd.edu>
2025-10-10 17:10:53 -0400

    added ISIC EDA

--- src/data-processor/processor_fitz.py
+++ src/data-processor/processor_fitz.py
@@ -77,5 +77,3 @@ if __name__ == "__main__":
     metadata_filt = dp.filter_metadata(metadata, FITZ_IMG_PATH)
     metadata_ins = dp.format_metadata_csv(metadata_filt, "fitzpatrick17k", FITZ_IMG_PATH)
     dp.update_data(metadata_ins, metadata_filt, "fitzpatrick17k_metadata.csv", FITZ_IMG_PATH)
-
-
